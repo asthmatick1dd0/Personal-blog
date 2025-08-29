bea13c1e82ab0ad6fa5a5d672c318fb3f9e4ac60
asthmatick1d <ansergoff@gmail.com>
2025-08-30 01:22:58 +0300

    Implement MongoDB connection and create Post model; update main route to fetch posts dynamically

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+
+// Connect to MongoDB
+connectDB();
 
 // Templating engine
 app.use(expressLayout);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post.js')
 
-router.get('', (req, res) => {
+
+/**
+ * GET /
+ * HOME
+ */
+
+router.get('', async (req, res) => {
     const locals = {
         title: "Blog",
         description: "Welcome to my blog"
     }
-    res.render('index', locals);
+    try {
+        const data = await Post.find();
+        res.render('index', { locals, data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
 });
+
+
+// function insertPostData() {
+//     Post.insertMany([
+//         {
+//             title: "Building a blog with Node.js",
+//             content: "In this post, we'll explore how to build a blog using Node.js and Express."
+//         }
+//     ])
+// };
+
+// insertPostData();
+
+
+
+
+
+
+
+
+
+
 
 router.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
